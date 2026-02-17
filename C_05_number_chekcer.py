769eedaf13e6139825e2cbc4cfdd8b2b61974128
SmartConnor <merigildod3205@masseyhigh.school.nz>
2026-02-18 09:47:08 +1300

    Add files via upload

---
+++ C_05_number_chekcer.py
@@ -0,0 +1,19 @@
+# Function go here...
+def num_checker(question):
+    """Checks users enter an integer between two values"""
+
+    error = "Oops - please enter an integer more than zero."
+
+    while True:
+
+        try:
+            # Change the response to an integer and check that it's more than zero
+            response = int(input(question))
+
+            if response > 0:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
